47f1f3b1e93b8bfafb9e4b038a4aeb4177be838f
Gus_Mustan <gustavohuaillaramos@gmail.com>
2025-07-20 16:26:08 -0500

    agregando print al fast

--- pruebas.py
+++ pruebas.py
@@ -6,4 +6,9 @@ print( "rama Gianfranco ")
 print("prueba de la rama pruebas")
 
 print("prueba 2 de la rama pruebas")
+<<<<<<< HEAD
 print("hola mundo")
+=======
+print("hola mundo")
+print("al fast pe ")
+>>>>>>> 85673dc (agregando print al fast)
