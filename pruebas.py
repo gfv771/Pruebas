d57db30928261531e8fdf12b9dd60ed1b1f5d5e4
GianfrancoFlores <gifranfv08@gmail.com>
2025-07-20 16:46:28 -0500

    formateo

--- pruebas.py
+++ pruebas.py
@@ -1,18 +1,1 @@
 print("HOLA MUNDO")
-
-
-print( "rama Gianfranco ")
-
-print("prueba de la rama pruebas")
-
-print("prueba 2 de la rama pruebas")
-
-print("hola mundo")
-
-print("hola mundo")
-
-print("no se ahh")
-print("pipipi")
-print("prueba flores ")
-
-print("Mis cheras")
